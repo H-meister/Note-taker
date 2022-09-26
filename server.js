878644689b59b25f74bdbe192de17759601a616d
H-meister <105766365+H-meister@users.noreply.github.com>
2022-09-26 13:30:36 -0400

    making working routes and deploying to heroku

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 //require stuff that the machine needs
-const fs = require('fs');
-const path = require('path');
+const htmlRoutes = require('./routes/htmlRoutes');
 const express = require('express')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
