d2d7254820bc845d119c38be83df384c52128838
H-meister <105766365+H-meister@users.noreply.github.com>
2022-09-28 23:24:15 -0400

    making everything work again and adding .json file

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 //require stuff that the machine needs
-const htmlRoutes = require('./routes/htmlRoutes');
-const express = require('express')
-
+const routes = require ('./routes/index');
+const express = require('express');
+//{PORT} requres port 80 for heroku or whetever else it requires if not default port 3001
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+//stuff I need to POST
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//to get everything including css when loading HTML from public folder
 app.use(express.static('public'));
-app.use('/', htmlRoutes);
-
+//my routes:)
+app.use('/', routes);
+//init the server on port 3001 or whatever it requires
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
