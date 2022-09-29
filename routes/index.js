3d5a8d5611ff2677e02959181919cae9eac9d372
H-meister <105766365+H-meister@users.noreply.github.com>
2022-09-28 23:56:41 -0400

    Final changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,23 @@
+//Things this app will require!
 const path = require('path');
 const router = require('express').Router();
+const fs = require('fs');
+const data = require('../db/db.json')
+//creating function to add notes~
+function createNote(body, noteList){
+    const newNote = body;
+    if(!Array.isArray(noteList))
+        noteList = [];
+    noteList.forEach(() => body.id = noteList.length);
 
+    noteList.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(noteList, null, 2)
+    );
+    return newNote;
+    
+}
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
@@ -11,11 +28,15 @@ router.get("/notes", (req, res) => {
 router.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'))
 });
+//this is to post notes
+router.post('/api/notes', (req, res) => {
+    const newNote = createNote(req.body, data);
+    res.json(newNote);
+});
 
-//if any other link is entered redirect to index.html
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-
+//sends it to server.js to read and execute
 module.exports = router;
